064eab1d22f04377053d6fbd2a477bed7de4da6a
kuldeepdiggibyte <kp.managuli2000@gmail.com>
2024-03-10 17:03:51 +0530

    comment added to else part

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 <h1>Commit inital</h1>
 <br><h2>Author</h2> 
 <p> Kuldeep Managoli</p>
+<p> Added to remove confilts</p>
--- python.py
+++ python.py
@@ -2,4 +2,4 @@ age = int(input("Enter your age"))
 if age >= 18:
     print("your are eligible to vote")
 else:
-    print("your are not eligible to vote")
+    print("your are not eligible to vote")#add comment
